393356c4b612746648c620f8d72c2e0d18695253
Satria Nur Firdaus <94550306+SatriaNurFirdaus7@users.noreply.github.com>
2022-03-15 21:25:54 +0700

    Add files via upload

---
+++ startbootstrap-sb-admin-gh-pages/startbootstrap-sb-admin-gh-pages/view/artikel1.php
@@ -0,0 +1,10 @@
+<?php
+include '../layout/header.php';
+?>
+
+<h1>Welcome To Website</h1>
+<p>Ini adalah Artikel</p>
+
+<?php
+include '../layout/include.php';
+?>
---
+++ web dinamis/artikel1.php
@@ -0,0 +1,10 @@
+<?php
+include 'header.php';
+?>
+
+<h1>Welcome To Website</h1>
+<p>Ini adalah Artikel</p>
+
+<?php
+include 'include.php';
+?>
